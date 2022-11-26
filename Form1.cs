e130066a745a9ce335b48e349f88acdf4c071a09
caarlosortegaa <118833961+caarlosortegaa@users.noreply.github.com>
2022-11-26 18:12:51 +0100

    practica3.1

---
+++ Form1.cs
@@ -0,0 +1,58 @@
+namespace telegrama
+
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void checkBox1_CheckedChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            
+            
+                string textoTelegrama;
+                char tipoTelegrama = 'o';
+                int numPalabras = 0;
+                double coste;
+                //Leo el telegrama
+                textoTelegrama = txtTelegrama.Text;
+                // telegrama urgente?
+             
+                    
+                //Obtengo el número de palabras que forma el telegrama
+               for(int i=0; i < textoTelegrama.Length; i++)
+               {
+                if (textoTelegrama[i] == ' ' || textoTelegrama[i] == '.')
+                {
+                    numPalabras++;
+                }
+               }
+                if (cbUrgente.Checked)
+                tipoTelegrama = 'u';
+            //Si el telegrama es ordinario
+            if (tipoTelegrama == 'o')
+                    if (numPalabras <= 10)
+                        coste = 2.5;
+                    else
+                        coste = 0.5 * numPalabras;
+                else
+                //Si el telegrama es urgente
+                if (tipoTelegrama == 'u')
+                    if (numPalabras <= 10)
+                        coste = 5;
+                    else
+                        coste = 5 + 0.75 * (numPalabras - 10);
+                else
+                    coste = 0;
+                txtPrecio.Text = coste.ToString() + " euros";
+            
+        }
+    }
+}
